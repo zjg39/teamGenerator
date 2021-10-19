71f5375be475c919390e61a0c7326f2276ef2bfb
Zac Goad <zjgoad@gmail.com>
2021-10-18 20:25:03 -0400

    remake format of project index

--- index.js
+++ index.js
@@ -1,213 +1,126 @@
+// The packages we need, the classes we need
 
 const inquirer = require("inquirer");
-const path = require("path");
+const writeFileAsync = util.promisify(fs.writeFile);
 const fs = require("fs");
+const util = require('util');
+
 const employee = require('./team/employee');
 const manager = require('./team/manager');
 const engineer = require('./team/engineer');
 const intern = require('./team/intern');
 
+
+// Array to store employee team
+
 let buildTeam = [];
 
-const employee = [
-    {
-        type: "list",
-        message: "Would you like to view info on your team?  Or add another team member?",
-        name: "add",
-        choices: ["View team info", "Add a team member"],
+
+// Begin prompt sequence for user
+
+function promptSequence() {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'position',
+            message: 'What kind of employee would you like to add to you team?',
+            choices: ['Intern', 'Engineer', 'Manager']
         }
-];
+    ]).then(response => {
+        if (response.position === 'Intern'){
+            internQuestions();
+        } else if (response.position === 'Engineer'){
+            engineerQuestions();
+        } else managerQuestions();
+    })
+}
 
-const position = [
-    {
-        type: "list",
-        message: "What is your position?",
-        name: "position",
-        choices: ["Intern", "Manager", "Engineer"],
-    }
-];
+// If the user chooses an Intern for the team, the following code is executed.
 
-
-const intern = [
-    {
-        type: "input",
-        message: "What is your intern's name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "What is your intern's ID number?",
-        name: "id",
-    },
-    {
-        type: "input",
-        message: "What is your intern's email address?",
-        name: "email",
-    },
-    {
-        type: "input",
-        message: "What school is your intern currently attending?",
-        name: "school",
-    },
-];
-
-const manager = [
-    {
-        type: "input",
-        message: "What is your manager's name?",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "What is your manager's ID number?",
-        name: "id",
-    },
-    {
-        type: "input",
-        message: "What is your manager's email address?",
-        name: "email",
-    },
-    {
-        type: "input",
-        message:"What is your manager's phone number?",
-        name: "phone",
-    },
-];
-
-const engineer = [
-    {
-        type: "input",
-        message: "What is your engineer's name?",
-        name: "name",
-    },
-    {
-        type:"input",
-        message: "What is your engineer's ID number?",
-        name: "id",
-    },
-    {
-        type: "input",
-        message: "What is your engineer's email address?",
-        name: "email",
-    },
-    {
-        type: "input",
-        message: "What is your engineer's GitHub ID?",
-        name: "gitHub",
-    },
-];
-
-function init() {
-    inquirer.prompt(employee).then((choices) => {
-        console.log(employee);
-        if(choices.add === "add a member to your team") {
-            addTeam();
+function internQuestions(){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the Intern?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employee ID of the Intern?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Which school did the Intern attend?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Intern\'s email address?'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: ' Would you like to add another employee to your team?',
+            choices: ['Intern', 'Engineer', 'Manager', 'I don\'t need another employee for my team.']
+        }
+    ]).then(response =>{
+        let addMemberIntern = new intern(response.name, response.id, response.email, response.school);
+        buildTeam.push(addMemberIntern);
+        if (response.addMember === 'Intern') {
+            internQuestions();
+        } else if (response.addMember === 'Engineer') {
+            engineerQuestions();
+        } else if (response.addMember === 'Manager') {
+            managerQuestions();
         } else {
-            makeHTML();
+            creatEmployeeCard();
         }
     })
-    };
+}
 
-    function addTeam() {
-        inquirer.prompt(position).then((choices) => {
-          if (choices.position === "Manager") {
-            inquirer.prompt(manager).then((answers) => {
-              let newManager = new Manager(
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.position,
-                answers.phone,
-              );
-              buildTeam.push(newManager);
-              init();
-            });
-          } else if (choices.position === "Engineer") {
-            inquirer.prompt(engineer).then((answers) => {
-              let newEngineer = new Engineer(
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.position,
-                answers.gitHubId,
-              );
-              buildTeam.push(newEngineer);
-              init();
-            });
-          } else if (choices.position === "Intern") {
-            inquirer.prompt(intern).then((answers) => {
-              let newIntern = new Intern(
-                answers.name,
-                answers.id,
-                answers.email,
-                answers.school,
-                answers.position,
-              );
-              buildTeam.push(newIntern);
-              init();
-            });
-          } else {
-              makeHTML();
-          }
-        });
-      }
+// If the user chooses an Engineer for the team, the following code is executed.
 
-      init();
-
-      function makeHTML (buildTeam) {
-        let renderPage;
-        for(i = 0; i < buildTeam.length; i++){
-            if (buildTeam[i].position == 'Manager'){
-                managerCard();
-            } else if (buildTeam[i].position == 'Intern') {
-                internCard();
-            } else if (buildTeam[i].position == 'Engineer') {
-                engineerCard();
-            }
+function engineerQuestions(){
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the Engineer?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the employee ID of the Engineer?'
+        },
+        {
+            type: 'input',
+            name: 'gitHubId',
+            message: 'What is the GitHub ID of the Engineer?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Engineer\'s email address?'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: ' Would you like to add another employee to your team?',
+            choices: ['Intern', 'Engineer', 'Manager', 'I don\'t need another employee for my team.']
         }
-      }
-
-      function managerCard(data){
-          var managerText = `<div class="row">
-          <div class="col s12">
-              <div class="card-content">
-                  <span class="title">${data.getPosition}</span>
-                  <p>Name: ${data.getName()}</p>
-                  <p>ID: ${data.getId()}</p>
-                  <p>Email: ${data.getEmail()}</p>
-              </div>
-          </div>
-      </div>`
-
-      renderPage = managerText;
-      }
-      function internCard(data){
-          var internText = `<div class="row">
-          <div class="col s12">
-              <div class="card-content">
-                  <span class="title">${data.getPosition}</span>
-                  <p>Name: ${data.getName()}</p>
-                  <p>ID: ${data.getId()}</p>
-                  <p>Email: ${data.getEmail()}</p>
-              </div>
-          </div>
-      </div>`
-
-      renderPage += internText;
-      }
-
-      function engineerCard(data){
-        var engineerText = `<div class="row">
-        <div class="col s12">
-            <div class="card-content">
-                <span class="title">${data.getPosition}</span>
-                <p>Name: ${data.getName()}</p>
-                <p>ID: ${data.getId()}</p>
-                <p>Email: ${data.getEmail()}</p>
-            </div>
-        </div>
-    </div>`
-
-    renderPage += engineerText;
-    }
-    return renderPage;
+    ]).then(response =>{
+        let addMemberIntern = new engineer(response.name, response.id, response.email, response.gitHubId);
+        buildTeam.push(addMemberIntern);
+        if (response.addMember === 'Intern') {
+            internQuestions();
+        } else if (response.addMember === 'Engineer') {
+            engineerQuestions();
+        } else if (response.addMember === 'Manager') {
+            managerQuestions();
+        } else {
+            creatEmployeeCard();
+        }
+    })
+}
